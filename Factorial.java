51daabc228d6ad9cf484c18478b649b5017ec4af
abhishek-ainapur1894 <abiainapur24@gmail.com>
2025-08-02 18:02:45 +0530

    some change in base case of factorial function

--- Factorial.java
+++ Factorial.java
@@ -2,8 +2,8 @@ public class Factorial
 {
     public static int fact(int n)
     {
-        if(n == 1)
-            return 1;
+        if(n == 1 || n == 0)
+            return n;
 
         return n * fact(n - 1);
     }    
